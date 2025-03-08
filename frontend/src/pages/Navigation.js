8fdcdb61b6b4b0c8e064896b419b287dde525b0e
guev06 <jojamisto@gmail.com>
2025-03-08 10:43:07 +0200

    changes class to className since react does not use class

--- frontend/src/pages/Navigation.js
+++ frontend/src/pages/Navigation.js
@@ -3,10 +3,10 @@ import {Link} from "react-router-dom"
 const Navigation = () => {
     return (
         <nav id="navbar">
-            <Link class="cat" to="/">Home-Quiz</Link> |
-            <Link class="cat" to="/List">All Albums</Link> |
-            <Link class="cat" to="/search">Search</Link> |
-            <Link class="cat" to="/add-album">Add Album</Link> |
+            <Link className="cat" to="/">Home-Quiz</Link> |
+            <Link className="cat" to="/List">All Albums</Link> |
+            <Link className="cat" to="/search">Search</Link> |
+            <Link className="cat" to="/add-album">Add Album</Link> |
             <img style={{width:100, background:"transparent"}}src="https://i.ibb.co/TMnX1NGk/alquizlogo.png" alt="Logo" />
 
         </nav>
